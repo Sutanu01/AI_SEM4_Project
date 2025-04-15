f0876c9069cf1e996bea111583790509d3fcb767
Sutanu <sutanuj11@gmail.com>
2025-04-15 22:02:57 +0530

    removed sample route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import MainPage from './pages/MainPage'
 import NotFound from './pages/NotFound'
-import Sample from './pages/Sample'
 
 const App = () => {
 	return (
@@ -11,7 +10,6 @@ const App = () => {
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/learning' element={<MainPage />} />
-				<Route path='/sample' element={<Sample />} />
 				<Route path='*' element={<NotFound />} />
 			</Routes>
 		</Router>
